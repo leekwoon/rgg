80cc4d03116c6faef24328da8b611daa74ae3d53
leekwoon <leekwoon@unist.ac.kr>
2023-10-05 18:58:59 +0900

    .

--- prepare_data.sh
+++ prepare_data.sh
@@ -9,6 +9,7 @@ gdown https://drive.google.com/uc?id=1bkL_LRKgPPJXNjvieR6ocGXn-KcQbD4Z
 tar -xvf data.tar
 rsync -av data/ logs/data
 rm data.tar
+rm data
 
 echo 
 echo Preparation complete!
--- prepare_pretrained_models.sh
+++ prepare_pretrained_models.sh
@@ -8,7 +8,8 @@ gdown https://drive.google.com/uc?id=1ElB0klbW3eDPehiRbdGd0fTmsulWGP8D
 
 tar -xvf gap_predictor.tar
 rsync -av gap_predictor/ logs/gap_predictor
-rm gap_predictor.tar
+rm gap_predictor.tar 
+rm gap_predictor
 
 echo 
 echo Preparation complete!
